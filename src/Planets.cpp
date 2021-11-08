3dd35c61ba671bcae357fadb9ff9af265c8172a9
natehalsey <halseyng@outlook.com>
2021-11-07 22:34:01 -0500

    renaming values, adding normalization to remain within 360 degrees

--- src/Planets.cpp
+++ src/Planets.cpp
@@ -1,4 +1,5 @@
 #include"Planets.h"
+#include<math.h>
 // a planet moves a certain number of degrees in a certain ammount of time
 // obviously a planet closer to the sun will move more in a given time than a planet further away
 // so it's really just a list of values with a transition function
@@ -20,7 +21,7 @@ Planets::Planets(){
     e = 0.205635     + 5.59E-10         * d    (Eccentricity)
     M = 168.6562_deg + 4.0923344368_deg * d    (Mean anonaly)
  */
-OrbitalElements Planets::computeMercuryData(double day){
+OrbitalElements Planets::computeMercuryOE(double day){
     double asc_node = 48.3313 + (3.24587E-5*day);
     double incl = 7.0047 + (5.00E-8*day);
     double arg = 29.1241 + (1.01444E-5*day);
@@ -28,7 +29,7 @@ OrbitalElements Planets::computeMercuryData(double day){
     double eccen = 0.205635 + (5.59E-10*day);
     double mean_anon = 168.6562+(4.0923344368*day);
 
-    return OrbitalElements(asc_node,incl,arg,axis,eccen,mean_anon);
+    return OrbitalElements(rev(asc_node),rev(incl),rev(arg),rev(axis),rev(eccen),rev(mean_anon));
 
 }
 /**
@@ -45,7 +46,7 @@ OrbitalElements Planets::computeMercuryData(double day){
     e = 0.006773     - 1.302E-9         * d
     M =  48.0052_deg + 1.6021302244_deg * d 
  */
-OrbitalElements Planets::computeVenusData(double day){
+OrbitalElements Planets::computeVenusOE(double day){
     double asc_node = 76.6799 + (2.46590E-5*day);
     double incl = 3.3946 + (2.75E-8*day);
     double arg = 54.8910 + (1.38374E-5*day);
@@ -53,7 +54,7 @@ OrbitalElements Planets::computeVenusData(double day){
     double eccen = 0.006773 - (1.302E-9*day);
     double mean_anon = 48.0052+(1.6021302244*day);
 
-    return OrbitalElements(asc_node,incl,arg,axis,eccen,mean_anon);
+    return OrbitalElements(rev(asc_node),rev(incl),rev(arg),rev(axis),rev(eccen),rev(mean_anon));
 }
 /**
  * @brief 
@@ -69,7 +70,7 @@ OrbitalElements Planets::computeVenusData(double day){
     e = 0.093405     + 2.516E-9         * d
     M =  18.6021_deg + 0.5240207766_deg * d
  */
-OrbitalElements Planets::computeMarsData(double day){
+OrbitalElements Planets::computeMarsOE(double day){
     double asc_node = 49.5574 + (2.11081E-5*day);
     double incl = 1.8497 - (1.78E-8*day);
     double arg = 286.5016 + (2.92961E-5*day);
@@ -77,9 +78,13 @@ OrbitalElements Planets::computeMarsData(double day){
     double eccen = 0.093405 + (2.516E-9 * day);
     double mean_anon = 18.6021 + (0.5240207766 * day);
 
-    return OrbitalElements(asc_node,incl,arg,axis,eccen,mean_anon);
+    return OrbitalElements(rev(asc_node),rev(incl),rev(arg),rev(axis),rev(eccen),rev(mean_anon));
 
 }
+double Planets::rev(double value){
+    return value - floor(value/360.0)*360.0;
+}
+/**
 OrbitalElements Planets::computeJupiterData(double day){
 
 }
@@ -92,34 +97,9 @@ OrbitalElements Planets::computeUranusData(double day){
 OrbitalElements Planets::computeNeptuneData(double day){
 
 }
+*/
 
 /**
-Mercury:
-
-    N =  48.3313_deg + 3.24587E-5_deg   * d    (Long of asc. node)
-    i =   7.0047_deg + 5.00E-8_deg      * d    (Inclination)
-    w =  29.1241_deg + 1.01444E-5_deg   * d    (Argument of perihelion)
-    a = 0.387098                               (Semi-major axis)
-    e = 0.205635     + 5.59E-10         * d    (Eccentricity)
-    M = 168.6562_deg + 4.0923344368_deg * d    (Mean anonaly)
-
-Venus:
-
-    N =  76.6799_deg + 2.46590E-5_deg   * d
-    i =   3.3946_deg + 2.75E-8_deg      * d
-    w =  54.8910_deg + 1.38374E-5_deg   * d
-    a = 0.723330
-    e = 0.006773     - 1.302E-9         * d
-    M =  48.0052_deg + 1.6021302244_deg * d
-
-Mars:
-
-    N =  49.5574_deg + 2.11081E-5_deg   * d
-    i =   1.8497_deg - 1.78E-8_deg      * d
-    w = 286.5016_deg + 2.92961E-5_deg   * d
-    a = 1.523688
-    e = 0.093405     + 2.516E-9         * d
-    M =  18.6021_deg + 0.5240207766_deg * d
 
 Jupiter:
 
