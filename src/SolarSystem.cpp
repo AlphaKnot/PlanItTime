42d3a83ae9c1054a9e161e56c0e67d11d7d5ed34
natehalsey <halseyng@outlook.com>
2021-11-08 16:17:49 -0500

    remove init, add update system

--- src/SolarSystem.cpp
+++ src/SolarSystem.cpp
@@ -9,6 +9,11 @@ SolarSystem::SolarSystem(int year, int month, int day, int hour, int minute){
 SolarSystem::~SolarSystem(){
     delete planets;
 }
+void SolarSystem::updateSystem(int year, int month, int day, int hour, int minute){
+    delete planets;
+    current_day = computeCurrentDay(year,month,day,hour,minute);
+    Planets *planets = new Planets(current_day);
+}
 double SolarSystem::computeCurrentDay (int year, int month, int day, int hour, int minute){
     return 367*year - 7 * ( year + (month+9)/12 ) / 4 - 3 * ( (year + (month-9)/7 ) / 100 + 1 ) / 4 + 275*month/9 + day - 730515;
 }
