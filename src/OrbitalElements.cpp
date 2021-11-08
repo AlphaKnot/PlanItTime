484597c02d6ae90affdad3779ab9059959651125
natehalsey <halseyng@outlook.com>
2021-11-07 22:26:51 -0500

    thinking about doing it this way...

---
+++ src/OrbitalElements.cpp
@@ -0,0 +1,31 @@
+#include"OrbitalElements.h"
+
+OrbitalElements::OrbitalElements(double asc_node, double incl, double arg, double axis, double eccen, double mean_anon){
+    m_asc_node = asc_node;
+    m_incl = incl;
+    m_arg = arg;
+    m_axis = axis;
+    m_eccen = eccen;
+    m_mean_anon = mean_anon;
+}
+OrbitalElements::~OrbitalElements(){
+    //pass
+}
+double OrbitalElements::getAscNode(){
+    return m_asc_node;
+}
+double OrbitalElements::getIncl(){
+    return m_incl;
+}
+double OrbitalElements::getArg(){
+    return m_arg;
+}
+double OrbitalElements::getAxis(){
+    return m_axis;
+}
+double OrbitalElements::getEccen(){
+    return m_eccen;
+}
+double OrbitalElements::getMeanAnon(){
+    return m_mean_anon;
+}
