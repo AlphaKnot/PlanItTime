2f0ce7a7be4f7255f0a6e7a8a8d853f1de29bc1a
natehalsey <halseyng@outlook.com>
2021-11-09 16:59:11 -0500

    main change

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "uiscreen.h"
 
 #include <QApplication>
@@ -9,28 +8,4 @@ int main(int argc, char *argv[])
     UIScreen w;
     w.show();
     return a.exec();
-=======
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-
-
-int main(int argc, char *argv[])
-{
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
-    QGuiApplication app(argc, argv);
-
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-
-    return app.exec();
->>>>>>> c7af9712ff1f3d279d7d026d3549d1e384b22ed3
 }
